d1132b83eb13656392d97e6c580f14e696611483
Hobin Yoon <hobinyoon@gmail.com>
2018-04-13 15:17:36 -0400

    plot whole foods locations

---
+++ misc/plot-whole-foods-locations/plot.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+
+import csv
+import geopy
+import os
+import sqlite3
+import sys
+import time
+
+sys.path.insert(0, "%s/work/cp-mec/util" % os.path.expanduser("~"))
+import Cons
+import Util
+
+import Conf
+
+
+def main(argv):
+  Conf.Init()
+  Plot()
+
+
+def Plot():
+  fn_in = GetPlotData()
+  fn_out = "%s.pdf" % fn_in
+
+  with Cons.MT("Plotting Whole Foods store locations ..."):
+    env = os.environ.copy()
+    env["FN_IN"] = fn_in
+    env["FN_OUT"] = fn_out
+    Util.RunSubp("gnuplot %s/whole-foods-locs.gnuplot" % os.path.dirname(__file__), env=env)
+    Cons.P("Created %s %d" % (fn_out, os.path.getsize(fn_out)))
+
+
+def GetPlotData():
+  fn_out = "%s/whole-foods-locations" % Conf.DnOut()
+  if os.path.exists(fn_out):
+    return fn_out
+
+  fn_in = Conf.Get("addrs")
+  with Cons.MT("Adding store locations from %s" % fn_in):
+    addrs = set()
+    num_dups = 0
+    with open(fn_in) as fo:
+      for line in fo:
+        addr = line.strip()
+        if len(addr) == 0:
+          continue
+        if addr[0] == "#":
+          continue
+        if addr in addrs:
+          #Cons.P("Dup: %s" % addr)
+          num_dups += 1
+        else:
+          addrs.add(addr)
+    Cons.P("Filtered out %d duplicate addresses" % num_dups)
+
+    db = WfLocDb()
+
+    geolocator = geopy.Nominatim()
+
+    for addr in sorted(addrs):
+      if db.Exist(addr):
+        continue
+      loc = None
+      try:
+        loc = geolocator.geocode(addr)
+      except geopy.exc.GeocoderTimedOut as e:
+        Cons.P("%s [%s]" % (e, addr))
+        sys.exit(1)
+
+      if loc is None:
+        raise RuntimeError("Unexpected: [%s]" % addr)
+
+      Cons.P("\"%s\" %f %f\n" % (addr, loc.latitude, loc.longitude))
+      db.Insert(addr, loc.latitude, loc.longitude)
+
+      # At most 1 req per second
+      #   https://operations.osmfoundation.org/policies/nominatim
+      time.sleep(1)
+
+  fn_in = Conf.Get("addrs_googlemaps")
+  with Cons.MT("Adding store locations from %s" % fn_in):
+      with open(fn_in) as fo:
+        for line in fo:
+          #Cons.P(line)
+          t = line.strip().split(" ")
+          addr = " ".join(t[0:-1])
+          t1 = t[-1].split(",")
+          lat = t1[0]
+          lon = t1[1]
+          #Cons.P("%s|%s,%s" % (addr, lat, lon))
+
+          if db.Exist(addr):
+            continue
+          db.Insert(addr, lat, lon)
+
+  with open(fn_out, "w") as fo:
+    fo.write("# addr lon lat\n")
+    for r in db.GetAll():
+      fo.write("\"%s\" %f %f\n" % (r["addr"], r["lat"], r["lon"]))
+  Cons.P("Created %s %d" % (fn_out, os.path.getsize(fn_out)))
+  return fn_out
+
+
+class WfLocDb:
+  def __init__(self):
+    self.conn = None
+
+    # Open or create DB
+    fn_db = Conf.GetFn("whole_foods_loc_db")
+    conn = None
+    if os.path.exists(fn_db):
+      with Cons.MT("Opening the existing db ..."):
+        conn = sqlite3.connect(fn_db)
+        if conn is None:
+          raise RuntimeError("Error! cannot create the database connection.")
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        q = "SELECT count(*) as cnt FROM whole_foods_loc"
+        cur.execute(q)
+        r = cur.fetchone()
+        Cons.P("There are %d records" % r["cnt"])
+    else:
+      with Cons.MT("Creating a new db ..."):
+        conn = sqlite3.connect(fn_db)
+        if conn is None:
+          raise RuntimeError("Error! cannot create the database connection.")
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        q = """CREATE TABLE IF NOT EXISTS whole_foods_loc (
+                 addr text NOT NULL
+                 , lat real NOT NULL
+                 , lon real NOT NULL
+                 , PRIMARY KEY (addr)
+               ); """
+        cur.execute(q)
+    self.conn = conn
+
+  def Exist(self, addr):
+    cur = self.conn.cursor()
+    cur.execute("SELECT count(*) as cnt FROM whole_foods_loc WHERE addr = '%s'" % addr)
+    r = cur.fetchone()
+    return (r["cnt"] == 1)
+
+  def Insert(self, addr, lat, lon):
+    cur = self.conn.cursor()
+    q = "INSERT INTO whole_foods_loc (addr, lat, lon) VALUES (?,?,?)"
+    cur.execute(q, (addr, lat, lon))
+    self.conn.commit()
+
+  def GetAll(self):
+    cur = self.conn.cursor()
+    cur.execute("SELECT addr, lat, lon FROM whole_foods_loc")
+    return cur.fetchall()
+
+#
+#    for db_type in ["RocksDB", "Mutant"]:
+#      fn = fn0 if db_type == "RocksDB" else fn1
+#      with open(fn) as fo:
+#        for line in fo:
+#          if line.startswith("#"):
+#            continue
+#          line = line.strip()
+#          t = re.split(r" +", line)
+#          hour = int(t[1].split(":")[0])
+#
+#          sst_id = t[7]
+#          # Ignore when end sst_id is -, which means an sstable was deleted.
+#          if sst_id == "-":
+#            continue
+#          sst_id = int(sst_id)
+#
+#          sst_size = int(t[8])
+#
+#          job_id = int(t[9])
+#
+#          # Creation reason: R, F, C, -
+#          cr = t[10]
+#          temp_triggered_single_sst_migr = (t[11] == "T")
+#          migr_dirc = t[12]
+#
+#          cur.execute(q, (fn, db_type, hour, sst_id, sst_size, job_id, cr, temp_triggered_single_sst_migr, migr_dirc))
+#    conn.commit()
+#    cur.close()
+#    return conn
+
+
+#def _OpenDB():
+#  with Cons.MT("Building a location DB ..."):
+#    fn_db = "%s/whole-foods-locations.db" % Conf.DnOut()
+#
+#    if os.path.exits(fn_db):
+#      raise RuntimeError("Implement")
+#    else:
+#      conn = sqlite3.connect(fn_db)
+#      if conn is None:
+#        raise RuntimeError("Error! cannot create the database connection.")
+#      cur = conn.cursor()
+#      table_schema = """CREATE TABLE IF NOT EXISTS whole_foods_loc (
+#                          addr text NOT NULL
+#                          , lon real NOT NULL
+#                          , lat real NOT NULL
+#                        ); """
+#      cur.execute(table_schema)
+#
+#    q = """INSERT INTO sst_creation_info (fn, db_type, hour, sst_id, sst_size, job_id, creation_reason, temp_triggered_single_sst_migr, migr_dirc)
+#             VALUES (?,?,?,?,?,?,?,?,?)"""
+#
+#    for db_type in ["RocksDB", "Mutant"]:
+#      fn = fn0 if db_type == "RocksDB" else fn1
+#      with open(fn) as fo:
+#        for line in fo:
+#          if line.startswith("#"):
+#            continue
+#          line = line.strip()
+#          t = re.split(r" +", line)
+#          hour = int(t[1].split(":")[0])
+#
+#          sst_id = t[7]
+#          # Ignore when end sst_id is -, which means an sstable was deleted.
+#          if sst_id == "-":
+#            continue
+#          sst_id = int(sst_id)
+#
+#          sst_size = int(t[8])
+#
+#          job_id = int(t[9])
+#
+#          # Creation reason: R, F, C, -
+#          cr = t[10]
+#          temp_triggered_single_sst_migr = (t[11] == "T")
+#          migr_dirc = t[12]
+#
+#          cur.execute(q, (fn, db_type, hour, sst_id, sst_size, job_id, cr, temp_triggered_single_sst_migr, migr_dirc))
+#    conn.commit()
+#    cur.close()
+#    return conn
+
+
+
+
+if __name__ == "__main__":
+  sys.exit(main(sys.argv))
---
+++ misc/plot-whole-foods-locations/whole-foods-locs.gnuplot
@@ -0,0 +1,37 @@
+#!/usr/bin/gnuplot
+#
+# Tested with gnuplot 4.6 patchlevel 6
+
+FN_IN = system("echo $FN_IN")
+FN_OUT = system("echo $FN_OUT")
+US_STATES_MAP = "~/work/cp-mec/resource/usa-map-smallsize-0.05"
+
+set print "-"
+#print sprintf("MAX_CLUSTER_SIZE=%f", MAX_CLUSTER_SIZE)
+
+#size_x=2.3
+size_x=3.0
+set terminal pdfcairo enhanced size (size_x)in, (size_x*0.75)in
+set output FN_OUT
+
+set lmargin 0
+set rmargin 0
+set tmargin 0
+set bmargin 0
+
+set xrange[-125:-66]
+set yrange [24:49.5]
+
+set noborder
+set notics
+
+PS=0.2
+
+# darkgreen
+LC="#006400"
+
+plot \
+FN_IN u 3:2 w p pt 7 ps PS lc rgb LC not, \
+US_STATES_MAP u 2:1 w filledcurves lw 1 fs transparent solid 0.0 border lc rgb "#808080" fc rgb "#FFFFFF" not
+
+# "with points" makes about 1/5 smaller file size than "with circles"
