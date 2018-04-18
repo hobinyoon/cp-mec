1c5816ec1feafb63fc0bc7f15af41b9e8cc30b58
Hobin Yoon <hobinyoon@gmail.com>
2018-04-18 10:55:31 -0400

    update

--- simulator/build-and-run.sh
+++ simulator/build-and-run.sh
@@ -13,8 +13,13 @@ mkdir -p $DN_BUILD
 pushd $DN_BUILD > /dev/null
 cmake ..
 #cmake -DCMAKE_BUILD_TYPE=Debug ..
-#time make -j
-time make
+
+if [ `hostname` == "u1604-16g" ]; then
+  time make
+else
+  time make -j
+fi
+
 popd > /dev/null
 
 printf "\n"
