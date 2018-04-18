22082c209b8dd14fb5b5c6fc523aea39a283f3e2
Hobin Yoon <hobinyoon@gmail.com>
2018-04-18 05:09:52 -0400

    speed up the loading time

--- simulator/src/youtube-access.cpp
+++ simulator/src/youtube-access.cpp
@@ -10,9 +10,6 @@
 #include "conf.h"
 #include "cons.h"
 
-// TODO: may not need this at all
-//#include "tweet.h"
-
 #include "util.h"
 #include "youtube-access.h"
 
@@ -24,11 +21,24 @@ namespace YoutubeAccess {
   // map<co_id, vector<video_id_accessed> >
   map<int, vector<string>* > _coid_accesses;
 
+  void _Load(const string& fn);
+  void _WriteCondensed(const string& fn);
+  bool _LoadCondensed(const string& fn);
+
+
   void Load() {
     string fn = Conf::GetFn("video_accesses_by_COs");
     if (! bf::exists(fn))
       THROW("Unexpected");
 
+    if (_LoadCondensed(fn))
+      return;
+    _Load(fn);
+    _WriteCondensed(fn);
+  }
+
+
+  void _Load(const string& fn) {
     Cons::MT _(boost::format("Loading YouTube video accesses by COs from %s ...") % fn);
 
     int max_co_id = atoi(Conf::GetStr("max_co_id").c_str());
@@ -80,11 +90,74 @@ namespace YoutubeAccess {
     Cons::P(boost::format("Loaded video access reqs from %d COs.") % _coid_accesses.size());
   }
 
+
+  void _WriteCondensed(const string& fn) {
+    bf::path p(fn);
+    string fn1 = str(boost::format("%s/%s") % Conf::DnOut() % p.filename().string());
+    if (bf::exists(fn1))
+      return;
+
+    Cons::MT _(boost::format("Generating file %s") % fn1);
+    ofstream ofs(fn1);
+    size_t n = _coid_accesses.size();
+		ofs.write((char*)&n, sizeof(n));
+    for (auto i: _coid_accesses) {
+      int co_id = i.first;
+      ofs.write((char*)&co_id, sizeof(co_id));
+
+      vector<string>* accesses = i.second;
+      size_t n = accesses->size();
+      ofs.write((char*)&n, sizeof(n));
+      for (auto& obj_id: *accesses)
+        Util::WriteStr(ofs, obj_id);
+    }
+    ofs.close();
+    Cons::P(boost::format("Created %s %d") % fn1 % boost::filesystem::file_size(fn1));
+  }
+
+
+  bool _LoadCondensed(const string& fn) {
+    bf::path p(fn);
+    string fn1 = str(boost::format("%s/%s") % Conf::DnOut() % p.filename().string());
+    if (! bf::exists(fn1))
+      return false;
+
+    Cons::MT _(boost::format("Loading condensed YouTube video accesses by COs from %s ...") % fn1);
+
+    int max_co_id = atoi(Conf::GetStr("max_co_id").c_str());
+
+    ifstream ifs(fn1);
+    size_t num_COs;
+    ifs.read((char*)&num_COs, sizeof(num_COs));
+    for (size_t i = 0; i < num_COs; i ++) {
+      int co_id;
+      ifs.read((char*)&co_id, sizeof(co_id));
+      if (max_co_id != -1 && max_co_id < co_id) {
+        Cons::P(boost::format("Passed max_co_id %d. Stop loading") % max_co_id);
+        break;
+      }
+
+      size_t num_accesses;
+      ifs.read((char*)&num_accesses, sizeof(num_accesses));
+      vector<string>* vids = new vector<string>();
+      for (size_t j = 0; j < num_accesses; j ++) {
+        string obj_id;
+        Util::ReadStr(ifs, obj_id);
+        vids->push_back(obj_id);
+      }
+      _coid_accesses.emplace(co_id, vids);
+    }
+    Cons::P(boost::format("Loaded video access reqs from %d COs.") % _coid_accesses.size());
+    return true;
+  }
+
+
   void FreeMem() {
     for (auto i: _coid_accesses)
       delete i.second;
     _coid_accesses.clear();
   }
+
 
   const map<int, vector<string>* >& CoAccesses() {
     return _coid_accesses;
